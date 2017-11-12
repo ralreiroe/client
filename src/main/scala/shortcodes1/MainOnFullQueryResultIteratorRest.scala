df369769e1d37ae15d6b7a11ee3737fb7c832149
Ralf Oenning <ralf@invicto.net>
2017-11-12 14:04:26 +0000

    Extracting the high-level steps of the execution into a single "pipeline line"
    On the left execution happens across all lines.
    On the right, it onl y happens on one line.
    
    This one line
    	- shows a pipeline
    	- gives names to the steps

--- src/main/scala/shortcodes1/MainOnFullQueryResultIteratorRest.scala
+++ src/main/scala/shortcodes1/MainOnFullQueryResultIteratorRest.scala
@@ -2,15 +2,31 @@ package shortcodes1
 
 import java.io.FileWriter
 
-trait QueryRunner {
-  def runQuery(queryType: Int = 1): Iterator[String]
+/**
+  * Can produce a query result for some query (just in int in this mock case)
+  */
+trait QueryResultProducingCapability {
+  def runQuery(queryType: Int = 1): Iterator[String] = {
+    val toAdd = queryType * 1000 //just to produce something queryType-dependent
+    (101 to 200).zip(201 to 300).map {
+      case (i, j) => {
+        println("producing query result line")
+        s"${toAdd + i},${toAdd + j}"
+      }
+    }
+  }.toIterator
 }
 
-class Producer {
-  //run query for querytypes 1,2,3
-  def cesiumOutput(cs: QueryRunner): Map[Int, Iterator[String]] = (for (qt <- Set(1,2,3)) yield (qt, cs.runQuery(qt))).toMap
+/**
+  * Runs a number of configured queries against Cesium - just a mock in this case. A real one would read queries from a config file
+  */
+class ProducerForAllConfigured {
+  def cesiumOutput(cs: QueryResultProducingCapability): Map[Int, Iterator[String]] = (for (qt <- Set(1,2,3)) yield (qt, cs.runQuery(qt))).toMap
 }
 
+/**
+  * Injects values from a query result into a string by replacing certain labels in the string
+  */
 object Injector {
 
   //1101,1201 and xetra,lc,sc makes xetra,1201,1101
@@ -42,7 +58,7 @@ injecting
 ...
 
   *
-  * Ie. all query result lines are produced upfront and into memeory. but
+  * Ie. all query result lines are produced upfront and into memory. but
   * injecting only happens if the final line is to be written to the file.
   *
   * So while the queryResult is fully loaded into memory, the injected lines
@@ -50,23 +66,23 @@ injecting
   */
 object MainOnFullQueryResultIteratorRest extends App {
 
-  val cesiumOutput: Map[Int, Iterator[String]] = (new Producer).cesiumOutput(new QueryRunner {
-    override def runQuery(queryType: Int) = {
-      val toAdd = queryType*1000  //just to produce something queryType-dependent
-      (101 to 200).zip(201 to 300).map{
-        case (i,j) => {
-          println("producing query result line")
-          s"${toAdd+i},${toAdd+j}"
-        }
-      }
-    }.toIterator
-  })
+//  val fullPipeline = toCesiumOutputs.andThen(toExchangeOutputs).andThen(toFiles)
+//  fullPipeline(new QueryResultProducingCapability {})
 
-  val exchangeOutput: Map[Int, Iterator[String]] = cesiumOutput.map {
+  //====Calling Functions====
+
+  toFiles.compose(toExchangeOutputs).compose(toCesiumOutputs).apply(new QueryResultProducingCapability {})
+
+
+  //====Declaring Functions===
+
+  lazy val toCesiumOutputs: QueryResultProducingCapability => Map[Int, Iterator[String]] = qr => (new ProducerForAllConfigured).cesiumOutput(new QueryResultProducingCapability {})
+
+  lazy val toExchangeOutputs: Map[Int, Iterator[String]] => Map[Int, Iterator[String]] = cesiumOutput => cesiumOutput.map {
     case (i, it) => (i, Injector.inject(it, "xetra,lc,sc\n"))
   }
 
-  exchangeOutput.foreach {
+  lazy val toFiles: Map[Int, Iterator[String]] => Unit = exchangeOutput => exchangeOutput.foreach {
     case (i,it) => {
 
       val fw = new FileWriter(s"${i}.txt", true)    //append=true
