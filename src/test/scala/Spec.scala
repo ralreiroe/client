7abf1c4950c34a1aff57144eea26f3545fc40c8a
Ralf Oenning <ralf@invicto.net>
2017-11-12 17:24:48 +0000

    Spec traits

---
+++ src/test/scala/Spec.scala
@@ -0,0 +1,11 @@
+import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
+
+trait Spec
+  extends FreeSpecLike
+    with MustMatchers
+    with DiagrammedAssertions
+    with TryValues
+    with OptionValues
+    with AppendedClues
+    with ScalaFutures
