c9a0bffa19a5f6b49a1bcc7da3589a4a78b57820
Ralf Oenning <ralf@invicto.net>
2018-01-06 19:29:39 +0000

    md5

--- src/test/scala/MD5.scala
+++ src/test/scala/MD5.scala
@@ -1,5 +1,7 @@
 import java.security.MessageDigest
 
+import utils.Spec
+
 class MD5 extends Spec {
 
   /**
@@ -14,5 +16,24 @@ class MD5 extends Spec {
     }
 
     println(getMd5("This is the input"))
+  }
+
+
+  "streaming" in {
+
+    import java.security.{MessageDigest, DigestInputStream}
+    import java.io.{File, FileInputStream}
+
+    // Compute a hash of a file
+    // The output of this function should match the output of running "md5 -q <file>"
+    def computeHash(path: String): String = {
+      val buffer = new Array[Byte](8192)
+      val md5 = MessageDigest.getInstance("MD5")
+
+      val dis = new DigestInputStream(new FileInputStream(new File(path)), md5)
+      try { while (dis.read(buffer) != -1) { } } finally { dis.close() }
+
+      md5.digest.map("%02x".format(_)).mkString
+    }
   }
 }
