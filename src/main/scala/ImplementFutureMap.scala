037bfd881aacc27f08793f470c2285d5f674a13a
Ralf Oenning <ralf@invicto.net>
2018-02-22 19:21:10 +0000

    working version with one callback int->Unit

--- src/main/scala/ImplementFutureMap.scala
+++ src/main/scala/ImplementFutureMap.scala
@@ -1,28 +1,39 @@
 import java.util.concurrent.atomic.AtomicReference
 
 import scala.annotation.tailrec
-import scala.concurrent.{ExecutionContext, ExecutionException, OnCompleteRunnable, Promise}
+import scala.concurrent._
 import scala.util.control.NonFatal
 import scala.util.{Failure, Success, Try}
 
 
+/**
+  * A runnable that executes the given func on a value
+  */
 class CallbackRunnable[T](val executor: ExecutionContext, val func: Try[T] => Any) extends Runnable with OnCompleteRunnable {
 
   var value: Try[T] = null
 
   override def run() = {
     require(value ne null)
-    try func(value) catch { case NonFatal(e) => executor reportFailure e }
+    try func(value) catch {
+      case NonFatal(e) => executor reportFailure e
+    }
   }
 
   def executeWithValue(v: Try[T]): Unit = {
     require(value eq null) // can't complete it twice
     value = v
-    try executor.execute(this) catch { case NonFatal(t) => executor reportFailure t }
+    try executor.execute(this) catch {
+      case NonFatal(t) => executor reportFailure t
+    }
   }
 }
 
-
+/**
+  * A promise is either completed or a list of runnables
+  *
+  * @tparam T
+  */
 class MyPromise[T] extends AtomicReference[AnyRef](Nil) {
   def future: this.type = this
 
@@ -31,55 +42,46 @@ class MyPromise[T] extends AtomicReference[AnyRef](Nil) {
     case _ => false
   }
 
-  final def tryComplete(value: Try[T]): Boolean = {
-    val resolved: Try[T] = resolveTry(value)
-    tryCompleteAndGetListeners(resolved) match {
-      case null             => false
-      case rs if rs.isEmpty => true
-      case rs               => rs.foreach(r => r.executeWithValue(resolved)); true
-    }
-  }
-
+  def registerCallback[U](func: Try[T] => U)(implicit executor: ExecutionContext) = runOnValueIfCompleteOtherwiseDefer(func)
+  def complete[U](value: Try[T])(implicit executor: ExecutionContext) = completeWithAndExecuteCallbacksOn(value)
 
   @tailrec
-  private def tryCompleteAndGetListeners(v: Try[T]): List[CallbackRunnable[T]] = {
-    get() match {
-      case raw: List[_] =>
-        val cur = raw.asInstanceOf[List[CallbackRunnable[T]]]
-        if (compareAndSet(cur, v)) cur else tryCompleteAndGetListeners(v)
-      case _ => null
-    }
-  }
+  final def runOnValueIfCompleteOtherwiseDefer[U](func: Try[T] => U)(implicit executor: ExecutionContext): Unit = {
 
-  final def onComplete[U](func: Try[T] => U)(implicit executor: ExecutionContext): Unit =
-    dispatchOrAddCallback(new CallbackRunnable[T](executor.prepare(), func))
+    val runnable = new CallbackRunnable[T](executor.prepare(), func)
 
-  @tailrec
-  private def dispatchOrAddCallback(runnable: CallbackRunnable[T]): Unit = {
     get() match {
-      case r: Try[_]          => runnable.executeWithValue(r.asInstanceOf[Try[T]])
+      case r: Try[_] => runnable.executeWithValue(r.asInstanceOf[Try[T]])
       case listeners: List[_] => if (compareAndSet(listeners, runnable :: listeners)) ()
-      else dispatchOrAddCallback(runnable)
+      else runOnValueIfCompleteOtherwiseDefer(func)
     }
+
   }
 
-  def complete(result: Try[T]): this.type =
-    if (tryComplete(result)) this else throw new IllegalStateException("Promise already completed.")
+  /**
+    * the secret to combining futures.
+    * the "parent" promise swaps list of deferred functions for value and triggers list execution
+    * @param v
+    */
+  @tailrec
+  final def completeWithAndExecuteCallbacksOn(v: Try[T]): Unit =
 
+    get() match {
 
-  private def resolveTry[T](source: Try[T]): Try[T] = source match {
-    case Failure(t) => resolver(t)
-    case _          => source
-  }
+      case raw: List[_] =>
+        val runnables = raw.asInstanceOf[List[CallbackRunnable[T]]]
 
-  private def resolver[T](throwable: Throwable): Try[T] = throwable match {
-    case t: scala.runtime.NonLocalReturnControl[_] => Success(t.value.asInstanceOf[T])
-    case t: scala.util.control.ControlThrowable    => Failure(new ExecutionException("Boxed ControlThrowable", t))
-    case t: InterruptedException                   => Failure(new ExecutionException("Boxed InterruptedException", t))
-    case e: Error                                  => Failure(new ExecutionException("Boxed Error", e))
-    case t                                         => Failure(t)
-  }
+        if (compareAndSet(runnables, v))
+          runnables.foreach(r => r.executeWithValue(v)) else completeWithAndExecuteCallbacksOn(v)
+
+      case _ => throw new IllegalStateException("Promise already completed.")
+    }
+
+  def map[S](f: Int => Int)(implicit executor: ExecutionContext): Future[S] = {null}
+
 }
+
+
 
 
 object ImplementFutureMap extends App {
@@ -87,7 +89,7 @@ object ImplementFutureMap extends App {
   import scala.concurrent.ExecutionContext.Implicits.global
 
   var i = 0
-  val f: Unit => Int = (x: Unit) => {
+  val f1: Unit => Int = (x: Unit) => {
     println(Thread.currentThread())
     while (i < 4) {
       Thread.sleep(1000);
@@ -98,27 +100,34 @@ object ImplementFutureMap extends App {
     333
   }
 
-  val f2: Try[Unit] => Try[Int] = (x: Try[Unit]) => x map f
+//  val p = MyFuture{ f1 }
+  val f1try: Try[Unit] => Try[Int] = (x: Try[Unit]) => x map f1
 
-  val secondPromise: MyPromise[Int] = new MyPromise[Int]()
+  val intPromise: MyPromise[Int] = new MyPromise[Int]()
 
   val startPromise = new MyPromise[Unit]()
-  startPromise.set(Success())
-  startPromise.onComplete((result: Try[Unit]) => {  //runs f2 on value in startPromise
+
+  startPromise.registerCallback((result: Try[Unit]) => { //runs f2 on value in startPromise
     val triedInt: Try[Int] = try {
-      f2(result)
+      f1try(result)
     } catch {
       case NonFatal(t) => Failure(t)
     }
-    secondPromise.complete(triedInt)    //complete and trigger secondPromise's callbacks
-  } )
+    intPromise.complete(triedInt) //complete and trigger secondPromise's callbacks
+  })
 
 
+  startPromise.complete(Success())
 
 
+  while (!intPromise.isCompleted) {
+    println(s"sleeping in ${Thread.currentThread()}")
+    Thread.sleep(1000)
+  }
 
-//  (new CallbackRunnable(global.prepare(), f2)).executeWithValue(Success())
+  println(intPromise.get())
 
-  Thread.sleep(50000)
+  //  (new CallbackRunnable(global.prepare(), f2)).executeWithValue(Success())
+
 
 }
