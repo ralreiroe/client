2b7417fdf0bd55dbd9cdaf7ee7373b1dc2af2faf
Ralf Oenning <ralf@invicto.net>
2020-02-07 12:52:36 +0000

    rest client gleif

--- src/main/scala/httpclient/Gleifcheck.scala
+++ src/main/scala/httpclient/Gleifcheck.scala
@@ -5,10 +5,9 @@ import java.io.File
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.DefaultHttpClient
 
-import scala.collection.immutable
 import scala.io.Source
 
-object ScalaApacheHttpRestClient2 {
+object Gleifcheck {
 
   /**
     * Returns the text content from a REST URL. Returns a blank String if there
@@ -35,14 +34,12 @@ object ScalaApacheHttpRestClient2 {
 
 //    val r: immutable.Seq[Int] = 1 to 7; r.iterator.zip(ints).foreach { case (l,r) => println(l+" "+r)}
 
-    val p = new java.io.PrintWriter(new File("btt.result.txt"))     //a printwriter without auto-flush
-    val notActiveOrIssued = Source.fromFile(new File("queryResult.51.bttleis.csv")).getLines.zip(ints).filter {
-      case (leistr, idx) =>
-        if (idx % 100 ==0) {
-          println(idx)
-          p.flush
-        }
-        Thread.sleep(5)
+    val resultFile = new java.io.PrintWriter(new File("lmeinvalidleis.result.txt"))     //a printwriter without auto-flush
+
+
+    val source = Source.fromFile(new File("lmeinvalidleis.txt")).getLines filter {
+      case leistr =>
+       Thread.sleep(5)
         val str = getRestContent("https://leilookup.gleif.org/api/v1/leirecords?lei=" + leistr.trim)
         val b = str.contains("ACTIVE") && str.contains("ISSUED")
         b
@@ -50,10 +47,10 @@ object ScalaApacheHttpRestClient2 {
 
 
     try {
-      notActiveOrIssued foreach p.println
+      source foreach resultFile.println
     } finally {
       println("closing file")
-      p.close
+      resultFile.close
     }
 
 
